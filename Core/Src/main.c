1247485d42d2791e134d93b00a5d4bd0d81e91b0
lebabach2004 <lebabach2004@gmail.com>
2025-11-27 14:39:02 +0700

    demo

---
+++ Core/Src/main.c
@@ -0,0 +1,355 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "string.h"
+#include "stdbool.h"
+#include "stdio.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart1;
+
+/* USER CODE BEGIN PV */
+char node_id[] = "0001"; 
+#define RX_BUF_SIZE 128
+volatile uint8_t  rx_byte;                    
+volatile uint8_t  rx_buf[RX_BUF_SIZE];        
+volatile uint16_t rx_idx = 0;
+volatile bool line_ready = false;   
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART1_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void LoRa_UART_Send(const char *str)
+{
+    HAL_UART_Transmit(&huart1, (uint8_t*)str, strlen(str), 1000);
+}
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+    if (huart->Instance == USART1 )
+    {
+			uint8_t ch = rx_byte;
+			if (!line_ready)  
+			{
+				if (rx_idx < RX_BUF_SIZE - 1)
+				{
+					rx_buf[rx_idx++] = ch;
+					if (ch == '\n')   
+					{
+						rx_buf[rx_idx] = '\0';
+						line_ready = true;
+					}
+				}
+				else
+				{
+					rx_idx = 0;
+				}
+			}
+			HAL_UART_Receive_IT(&huart1, (uint8_t*)&rx_byte, 1);
+    }
+}
+// Cho 1 dong moi tu RX (toi '\n') voi timeout_ms
+// copy vào buf (size maxlen), return: so byte (0 = timeout)
+int LoRa_WaitLine(char *buf, uint16_t maxlen, uint32_t timeout_ms)
+{
+		uint32_t start = HAL_GetTick();
+    while ((HAL_GetTick() - start) < timeout_ms)
+    {
+			if (line_ready)
+			{
+				// copy safely
+				uint16_t len = rx_idx;
+				if (len >= maxlen) len = maxlen - 1;
+				memcpy(buf, (const void*)rx_buf, len);
+				// reset trang thái cho dong sau
+				rx_idx = 0;
+				line_ready = false;
+				return len;
+			}
+    }
+    return 0;  // timeout
+}
+#define MAX_SEND_RETRIES        50
+#define SEND_RESP_TIMEOUT_MS    2000  // 2s cho OK
+
+bool request_send_permission(void)
+{
+    char line[64];
+    for (int attempt = 0; attempt < MAX_SEND_RETRIES; attempt++)
+    {
+			// 1) Gui SEND|id
+			char msg[32];
+			snprintf(msg, sizeof(msg), "SEND|%s\r\n", node_id);
+			LoRa_UART_Send(msg);
+			// 2) Cho OK|id
+			int len = LoRa_WaitLine(line, sizeof(line), SEND_RESP_TIMEOUT_MS);
+			if (len > 0)
+			{
+				// vi du: "OK|0001\r\n"
+				if (strncmp(line, "OK|", 3) == 0)
+				{
+					char id[8] = {0};
+					if (sscanf(line + 3, "%7s", id) == 1){
+						if (strcmp(id, node_id) == 0){
+							// Duoc phep gui Data
+							return true;
+						}
+					}
+				}
+			}
+      HAL_Delay(200);
+    }
+    return false; 
+}
+#define MAX_DATA_RETRIES        50
+#define DATA_ACK_TIMEOUT_MS     2000  // 2s cho ACK
+
+bool send_data_with_ack(float hum, float temp)
+{
+    char line[64];
+    for (int attempt = 0; attempt < MAX_DATA_RETRIES; attempt++)
+    {
+			// 1) Gui DATA|id|Hum: .. Tmp: ..
+			char msg[64];
+			snprintf(msg, sizeof(msg), "DATA|%s|Hum: %.1f Tmp: %.1f\r\n", node_id, hum, temp);
+			LoRa_UART_Send(msg);
+			// 2) Cho ACK|id
+			int len = LoRa_WaitLine(line, sizeof(line), DATA_ACK_TIMEOUT_MS);
+			if (len > 0)
+      {
+				// vi du: "ACK|0001\r\n"
+				if (strncmp(line, "ACK|", 4) == 0){
+					char id[8] = {0};
+					if (sscanf(line + 4, "%7s", id) == 1){
+						if (strcmp(id, node_id) == 0){
+							return true;  // gui ok
+						}
+					}
+				}
+			}
+        HAL_Delay(200); // retry
+    }
+    return false;
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+	HAL_UART_Receive_IT(&huart1, (uint8_t*)&rx_byte, 1);
+	float t = 25.0f;
+	float h = 60.0f;
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+		// 1) Xin quyen gui
+		bool can_send = request_send_permission();
+		if(can_send){
+			// 2) Gui DATA & cho ACK
+			bool sent_ok = send_data_with_ack(h, t);
+		}
+		HAL_Delay(50000);
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 9600;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
